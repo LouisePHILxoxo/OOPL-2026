2b048afd13585f913aa10e8528eeb8abdf1b6c74
LouisePHILxoxo <janlouise.calacsan@lorma.edu>
2026-01-15 12:08:53 +0800

    Main

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Phone obj1 = new Phone();
+        obj1.displaySize = "6.1 OLED Super Retina Display";
+        obj1.storage = 128;
+
+        var obj2 = new Phone();
+        obj2.displaySize = "5.2 IPS-LCD Display";
+        obj2.storage = 64;
+
+        obj1.printSpecs();
+        obj2.printSpecs();
+    }
+    
+}
---
+++ Phone.java
@@ -0,0 +1,10 @@
+public class Phone {
+    String displaySize;
+    double storage;
+
+    void printSpecs(){
+        System.out.printf("""
+                %s %s
+                """, displaySize, storage);
+    }
+}
